c6a3be3f1ae0bb5f18389849f70fe4cd9c3b1743
ione96 <80782468+ione96@users.noreply.github.com>
2025-09-26 01:17:11 +0500

    rename to Power of Thor - Episode 1.cpp

--- Power of Thor - Episode 1.cpp
+++ Power of Thor - Episode 1.cpp
@@ -1,4 +1,3 @@
-'''C++
 // Power of Thor - Episode 1
 // A basic problem to go a little bit further with conditions and variables: your program must allow Thor to reach the coordinates of the light of power in a 2D field.
 
